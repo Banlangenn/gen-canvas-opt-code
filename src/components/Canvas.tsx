4c67837750b3706fd49ccd484770711650691f80
qiuxchao <2915241235@qq.com>
2023-02-13 15:32:48 +0800

    🧩 update: 事件模型

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { isEqual } from 'lodash';
 import { useCanvasStore } from '../store';
 import { ComponentType } from '../types';
@@ -9,31 +10,146 @@ interface PropsType {
 	style: React.CSSProperties;
 }
 
+interface ElTargetType extends HTMLElement {
+	dataset: { type: OperationElType['type'] };
+}
+
+interface OperationElType {
+	/** 操作类型 移动/线/点 */
+	type:
+		| ''
+		| 'move'
+		| 'line-top'
+		| 'line-bottom'
+		| 'line-left'
+		| 'line-right'
+		| 'pointer-top-left'
+		| 'pointer-top-right'
+		| 'pointer-bottom-left'
+		| 'pointer-bottom-right';
+	/** 鼠标按下时的 x 坐标 */
+	mouseX: number;
+	/** 鼠标按下时的 y 坐标 */
+	mouseY: number;
+}
+const operationElDefault: OperationElType = {
+	type: '',
+	mouseX: 0,
+	mouseY: 0,
+};
+
 /** 画布 */
 const Canvas = ({ style }: PropsType) => {
 	const elList = useCanvasStore((state) => state.elList, isEqual);
 	const activeEl = useCanvasStore((state) => state.activeEl, isEqual);
-	const { updateEl, addEl } = useCanvasStore((state) => ({
+	const { updateEl, addEl, updateActiveEl } = useCanvasStore((state) => ({
 		updateEl: state.updateEl,
 		addEl: state.addEl,
+		updateActiveEl: state.updateActiveEl,
 	}));
+
+	// 当前操作的元素信息
+	const crtOperElRef = useRef<OperationElType>(operationElDefault);
 
 	// 放置组件
 	const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
 		event.preventDefault();
 		event.stopPropagation();
 		const type = event.dataTransfer.getData('type') as ComponentType;
+		if (!type) return;
 		const option = getElDefaultOpt(type, elList.length);
 		addEl(option);
 		updateEl(option);
+	};
+
+	// 鼠标按下：记录当前操作类型、元素坐标点和鼠标坐标点
+	const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+		const target = e.target as ElTargetType;
+		const { type } = target.dataset;
+		if (!type) return;
+		crtOperElRef.current = {
+			type,
+			mouseX: e.clientX,
+			mouseY: e.clientY,
+		};
+		console.log(crtOperElRef.current);
+	};
+
+	// 鼠标移动：计算偏移量 根据操作类型确定修改元素的哪些样式
+	const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+		if (!crtOperElRef.current.type) return;
+		// 计算鼠标偏移值
+		const offsetX = e.clientX - crtOperElRef.current.mouseX;
+		const offsetY = e.clientY - crtOperElRef.current.mouseY;
+		// 根据不同操作类型设置不同属性
+		switch (crtOperElRef.current.type) {
+			// 高度变化 top 变化
+			case 'line-top': {
+				break;
+			}
+			// 高度变化
+			case 'line-bottom': {
+				break;
+			}
+			// 宽度变化 left 变化
+			case 'line-left': {
+				break;
+			}
+			// 宽度变化
+			case 'line-right': {
+				break;
+			}
+			// 宽高变化 top left 变化
+			case 'pointer-top-left': {
+				break;
+			}
+			// 宽高变化 top 变化
+			case 'pointer-top-right': {
+				break;
+			}
+			// 宽高变化 left 变化
+			case 'pointer-bottom-left': {
+				break;
+			}
+			// 宽高变化
+			case 'pointer-bottom-right': {
+				break;
+			}
+			// 元素移动 top left 变化
+			case 'move': {
+				break;
+			}
+		}
+		const left = (activeEl?.height || 0) + offsetY;
+		const top = (activeEl?.height || 0) + offsetY;
+		console.log(offsetX, offsetY);
+	};
+
+	// 鼠标松开：清空记录的数据
+	const handleMouseUp = () => {
+		if (crtOperElRef.current.type) {
+			crtOperElRef.current = { ...operationElDefault };
+		}
+	};
+
+	// 鼠标离开画布：清空记录的数据
+	const handleMouseLeave = () => {
+		if (crtOperElRef.current.type) {
+			crtOperElRef.current = { ...operationElDefault };
+		}
 	};
 
 	return (
 		<div
 			className="bg-white shadow-[2px_2px_20px_0_rgba(0,0,0,0.25)] overflow-hidden relative"
 			style={style}
+			draggable="false"
 			onDragOver={(e) => e.preventDefault()}
 			onDrop={handleDrop}
+			onMouseDown={handleMouseDown}
+			onMouseMove={handleMouseMove}
+			onMouseUp={handleMouseUp}
+			onMouseLeave={handleMouseLeave}
 		>
 			{elList.map((el, index) =>
 				el.internal.id === activeEl?.internal.id ? null : (
--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react';
 import { useCanvasStore } from '../store';
 import { ComponentType, ComponentUniType } from '../types';
 import { setElOpt } from '../utils';
@@ -27,45 +26,9 @@ interface ComponentPropsType {
 	isActive?: boolean;
 }
 
-interface OperationElType {
-	/** 操作类型 移动/线/点 */
-	type:
-		| ''
-		| 'move'
-		| 'line-top'
-		| 'line-bottom'
-		| 'line-left'
-		| 'line-right'
-		| 'pointer-top-left'
-		| 'pointer-top-right'
-		| 'pointer-bottom-left'
-		| 'pointer-bottom-right';
-	/** 鼠标按下时的 x 坐标 */
-	mouseX: number;
-	/** 鼠标按下时的 y 坐标 */
-	mouseY: number;
-	/** 拖放元素的 x 坐标 */
-	offsetX: number;
-	/** 拖放元素的 y 坐标 */
-	offsetY: number;
-}
-const operationElDefault: OperationElType = {
-	type: '',
-	mouseX: 0,
-	mouseY: 0,
-	offsetX: 0,
-	offsetY: 0,
-};
-
 /** 画布中的组件 */
 const Component = ({ options, index, isActive }: ComponentPropsType) => {
-	const { updateEl, updateActiveEl } = useCanvasStore((state) => ({
-		updateActiveEl: state.updateActiveEl,
-		updateEl: state.updateEl,
-	}));
-
-	// 当前操作的元素信息
-	const crtOperElRef = useRef<OperationElType>(operationElDefault);
+	const updateEl = useCanvasStore((state) => state.updateEl);
 
 	// 样式
 	const style: React.CSSProperties = {
@@ -89,20 +52,6 @@ const Component = ({ options, index, isActive }: ComponentPropsType) => {
 		if (isActive) return;
 		updateEl({ ...options });
 	};
-	// 鼠标按下：记录当前操作类型、元素坐标点和鼠标坐标点
-	const handleMouseDown = (e: any) => {
-		const { type } = e.target.dataset;
-	};
-	// 鼠标移动：计算偏移量 根据操作类型确定修改元素的哪些样式
-	const handleMouseMove = (e: any) => {
-		if (!crtOperElRef.current.type) return;
-	};
-	// 鼠标松开：清空记录的数据
-	const handleMouseUp = () => {
-		if (crtOperElRef.current.type) {
-			crtOperElRef.current = operationElDefault;
-		}
-	};
 	return (
 		<Element
 			type={options.type}
@@ -110,44 +59,50 @@ const Component = ({ options, index, isActive }: ComponentPropsType) => {
 			className={`${!isActive && 'hover:el-active'}`}
 			{...specific}
 			draggable="false"
+			data-type="move"
 			onClick={handleClick}
-			onMouseDown={handleMouseDown}
-			onMouseMove={handleMouseMove}
-			onMouseUp={handleMouseUp}
 			children={
 				isActive && (
 					<>
 						<div
 							className="line line-top cursor-ns-resize"
 							data-type="line-top"
+							draggable="false"
 						></div>
 						<div
 							className="line line-bottom cursor-ns-resize"
 							data-type="line-bottom"
+							draggable="false"
 						></div>
 						<div
 							className="line line-left cursor-ew-resize"
 							data-type="line-left"
+							draggable="false"
 						></div>
 						<div
 							className="line line-right cursor-ew-resize"
 							data-type="line-right"
+							draggable="false"
 						></div>
 						<div
 							className="pointer pointer-top-left cursor-nwse-resize"
 							data-type="pointer-top-left"
+							draggable="false"
 						></div>
 						<div
 							className="pointer pointer-top-right cursor-nesw-resize"
 							data-type="pointer-top-right"
+							draggable="false"
 						></div>
 						<div
 							className="pointer pointer-bottom-left cursor-nesw-resize"
 							data-type="pointer-bottom-left"
+							draggable="false"
 						></div>
 						<div
 							className="pointer pointer-bottom-right cursor-nwse-resize"
 							data-type="pointer-bottom-right"
+							draggable="false"
 						></div>
 					</>
 				)
