210029b3361176dfde091c8a2f8e895617cd2777
qiuxchao <2915241235@qq.com>
2023-02-18 20:21:37 +0800

    🧩 update: 注释

--- src/store/index.ts
+++ src/store/index.ts
@@ -41,7 +41,6 @@ export const useCanvasSizeStore = create<CanvasSizeStoreType>()(
  * 更新状态：
  * - 当激活某个组件时，将该组件状态备份到 `activedEl`，页面中使用 `activedEl` 渲染该组件，这样当改变该组件的状态时，不需要更新整个列表
  * - 当有新的组件激活时，将上一个激活的组件同步到组件列表中
- * - 当导出代码时，将当前激活的组件同步到组件列表，取消当前组件的激活状态
  */
 interface CanvasStoreType {
 	/** 画布中的组件列表 */
